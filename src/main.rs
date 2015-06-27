a6037aa251bb2bc1782a8105bc880053066bb883
Keenan Lidral-Porter <klp2012@gmail.com>
2015-06-26 23:23:57 -0700

    [feat] creates simple enigma machine using mio (plain -> cipher)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
 extern crate mio;
-extern crate cipha;
+extern crate cipher;
+use mio::*;
+use cipher::*;
 fn main() {
-    println!("Hello, world!");
+    let mut event_loop = EventLoop::new().unwrap();
+    let sender = event_loop.channel();
+    struct GhostStruct;
+    impl Handler for GhostStruct {
+        type Timeout = ();
+        type Message = &'static str;
+
+        fn notify(&mut self,
+                   event_loop: &mut EventLoop<GhostStruct>,
+                   plaintext: &str) {
+            println!("Received Message!\n{:?}", plaintext);
+            let ciphertext = feistel_encrypt(plaintext, 1485832, 33);
+            println!("Encrypted Message!\n{:?}", ciphertext);
+            event_loop.shutdown();
+        }
+    }
+
+    // here we feed messages into the enigma machine
+    let response = sender.send("Oh wow this is transcendent");
+
+    if response.is_err() {
+        panic!("EEK, error!\n\t{:?}", response);
+    } else {
+        println!("yo\n\t{:?}", response);
+    }
+
+    event_loop.run(&mut GhostStruct).unwrap();
 }
