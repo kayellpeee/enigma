19b3219e5ac5259f18fdad45500984f38be57be5
Keenan Lidral-Porter <klp2012@gmail.com>
2015-06-28 14:34:56 -0700

    [feat] listening on port 3000 for connections

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,25 @@
 extern crate mio;
 extern crate cipher;
-use mio::*;
-use mio::tcp::TcpListener;
-use cipher::*;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 fn main() {
-    const SERVER: Token = Token(0);
-    const CLIENT: Token = Token(1);
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
 
-    let address = "127.0.0.1:3434".parse().unwrap();
-    let server = tcp::listen(&address).unwrap();
-
-    let mut event_loop = EventLoop::new().unwrap();
-    event_loop.register(&server, SERVER).unwrap();
-
-    let (sock, _) = tcp::connect(&address).unwrap();
-    event_loop.register(&sock, CLIENT).unwrap();
-
-    struct GhostStruct(NonBlock<TcpListener>);
-    impl Handler for GhostStruct {
-        type Timeout = ();
-        type Message = ();
-
-        fn readable(&mut self,
-                    event_loop: &mut EventLoop<GhostStruct>,
-                    token: Token,
-                    read: ReadHint) {
-            match token {
-                SERVER => {
-                    println!("Matched server, readhint: {:?}", read);
-                    let GhostStruct(ref mut server) = *self;
-                    let _ = server.accept();
-                }
-                CLIENT => {
-                    println!("Matched client, readhint: {:?}", read);
-                    event_loop.shutdown();
-                }
-                _ => panic!("Umatched Token! readhint: {:?}", read)
-            }
-        }
+    fn print_stream(stream: TcpStream) {
+        println!("check this {:?}", stream);
     }
 
-    event_loop.run(&mut GhostStruct(server)).unwrap();
+    println!("lister {:?}", listener);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("Spawning thread...");
+                thread::spawn(move || {
+                    print_stream(stream);
+                });
+            }
+            Err(e) => { panic!("some error {:?}", e); }
+        }
+    }
 }
