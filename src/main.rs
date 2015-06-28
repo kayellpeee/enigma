3d613337ab71596bd7172f55b4411bc6f2aba037
Keenan Lidral-Porter <klp2012@gmail.com>
2015-06-28 14:21:24 -0700

    [feat?] implements simple tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,44 @@
 extern crate mio;
 extern crate cipher;
 use mio::*;
+use mio::tcp::TcpListener;
 use cipher::*;
 fn main() {
+    const SERVER: Token = Token(0);
+    const CLIENT: Token = Token(1);
+
+    let address = "127.0.0.1:3434".parse().unwrap();
+    let server = tcp::listen(&address).unwrap();
+
     let mut event_loop = EventLoop::new().unwrap();
-    let sender = event_loop.channel();
-    struct GhostStruct;
+    event_loop.register(&server, SERVER).unwrap();
+
+    let (sock, _) = tcp::connect(&address).unwrap();
+    event_loop.register(&sock, CLIENT).unwrap();
+
+    struct GhostStruct(NonBlock<TcpListener>);
     impl Handler for GhostStruct {
         type Timeout = ();
-        type Message = &'static str;
+        type Message = ();
 
-        fn notify(&mut self,
-                   event_loop: &mut EventLoop<GhostStruct>,
-                   plaintext: &str) {
-            println!("Received Message!\n{:?}", plaintext);
-            let ciphertext = feistel_encrypt(plaintext, 1485832, 33);
-            println!("Encrypted Message!\n{:?}", ciphertext);
-            event_loop.shutdown();
+        fn readable(&mut self,
+                    event_loop: &mut EventLoop<GhostStruct>,
+                    token: Token,
+                    read: ReadHint) {
+            match token {
+                SERVER => {
+                    println!("Matched server, readhint: {:?}", read);
+                    let GhostStruct(ref mut server) = *self;
+                    let _ = server.accept();
+                }
+                CLIENT => {
+                    println!("Matched client, readhint: {:?}", read);
+                    event_loop.shutdown();
+                }
+                _ => panic!("Umatched Token! readhint: {:?}", read)
+            }
         }
     }
 
-    // here we feed messages into the enigma machine
-    let response = sender.send("Oh wow this is transcendent");
-
-    if response.is_err() {
-        panic!("EEK, error!\n\t{:?}", response);
-    } else {
-        println!("yo\n\t{:?}", response);
-    }
-
-    event_loop.run(&mut GhostStruct).unwrap();
+    event_loop.run(&mut GhostStruct(server)).unwrap();
 }
