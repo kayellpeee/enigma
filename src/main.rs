d6f0d02630f788fb91d8e7c8a3a22ac40b001555
Keenan Lidral-Porter <klp2012@gmail.com>
2015-06-29 23:24:24 -0700

    [1/2feat] spawns new thread to encrypt on new connections

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ extern crate mio;
 extern crate cipher;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+use cipher::*;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
 
     fn print_stream(stream: TcpStream) {
         println!("check this {:?}", stream);
+        let plaintext = "I want this to be a dynamic message :(";
+        let ciphertext = feistel_encrypt(plaintext, 141423, 7);
+        println!("finsihed encryption {:?}", ciphertext);
     }
 
     println!("lister {:?}", listener);
